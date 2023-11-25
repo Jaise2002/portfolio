a368ccf9986b1b613897901ed1d11745977ba5ac
jaise2002 <jaise.george@msds.christuniversity.in>
2023-11-25 11:37:48 +0530

    fullstack

---
+++ day2/newstyle.js
@@ -0,0 +1,11 @@
+function equation(val){
+document.getElementById("txt1").value+=val;
+
+}
+function calculator(val){
+    var a=document.getElementById("txt1").value;
+var c=eval(a)
+}
+function clearC{
+    document.getElementById("txt1").value=""
+}
